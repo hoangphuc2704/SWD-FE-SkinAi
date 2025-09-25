c266b75f011a66ad264958d089d3d2b1035c6e71
hoangphuc2704 <hoangphuc27042004@gmail.com>
2025-09-26 06:21:04 +0700

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import '../src/assets/styles/_global.module.scss';
+import '../src/assets/styles/_global.scss';
 import Header from './components/header/Header';
 import Home from './pages/home/Home';
 import AIPage from './pages/aiPage/AIPage';
